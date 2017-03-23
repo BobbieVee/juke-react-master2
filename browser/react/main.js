0c60f4453054c37ddf9ded2e63d8cea6149e535d
Robert van der Mark <robert.vandermark@gmail.com>
2017-03-23 18:47:32 -0400

    sidebar and footer

--- browser/react/main.js
+++ browser/react/main.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import Sidebar from './SideBar';
+import Footer from './Footer';
 
 export default class Main extends React.Component  {
+	constructor(props) {
+		super(props);
+		this.state = {name: 'Bobby'};
+	}
 	render(){
 		return(
 			<div id="main" className="container-fluid">
-				<h1>Hello world!</h1>
+			< Sidebar />
+			< Footer />
 			</div>
 		)
 	}
