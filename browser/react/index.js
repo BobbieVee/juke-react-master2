2cc340070c5297a7a9117af4876cbca8129c35c7
Robert van der Mark <robert.vandermark@gmail.com>
2017-03-23 15:35:10 -0400

    initial commit

---
+++ browser/react/index.js
@@ -0,0 +1,13 @@
+// 'use strict';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Main from './main';
+
+ReactDOM.render(
+	<div>< Main /></div>, 
+	document.getElementById('app')
+);
+
+
---
+++ browser/react/main.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class Main extends React.Component  {
+	render(){
+		return(
+			<div id="main" className="container-fluid">
+				<h1>Hello world!</h1>
+			</div>
+		)
+	}
+}
