2b74e1945775de60424ade6fa598d0dc476cbf7a
Robert van der Mark <robert.vandermark@gmail.com>
2017-03-25 20:26:32 -0400

    Pull data from database

--- browser/react/main.js
+++ browser/react/main.js
@@ -3,70 +3,26 @@ import Sidebar from './SideBar';
 import Footer from './Footer';
 import Album from './Album';
 import Albums from './Albums';
-
-
-const fakeAlbums = [
-	  {
-	    name: 'Abbey Road',
-	    id: 1,
-	    imageUrl: 'http://fillmurray.com/300/300',
-	    songs: [
-	      {
-	        id: 1,
-	        name: 'Romeo & Juliette',
-	        artists: [ 
-	          { name: 'Bill' } 
-	        ],
-	        genre: 'Funk',
-	        audioUrl: 'https://learndotresources.s3.amazonaws.com/workshop/5616dbe5a561920300b10cd7/Dexter_Britain_-_03_-_The_Stars_Are_Out_Interlude.mp3'
-	      }, 
-	      {
-	        id: 2,
-	        name: 'White Rabbit',
-	        artists: [
-	          { name: 'Bill' }, 
-	          { name: 'Alice' }
-	        ],
-	        genre: 'Fantasy',
-	        audioUrl: 'https://learndotresources.s3.amazonaws.com/workshop/5616dbe5a561920300b10cd7/Dexter_Britain_-_03_-_The_Stars_Are_Out_Interlude.mp3'
-	      }, 
-	      {
-	        id: 3,
-	        name: 'Lucy in the Sky with Diamonds',
-	        artists: [ 
-	          { name: 'Bob' } 
-	        ],
-	        genre: 'Space',
-	        audioUrl: 'https://learndotresources.s3.amazonaws.com/workshop/5616dbe5a561920300b10cd7/Dexter_Britain_-_03_-_The_Stars_Are_Out_Interlude.mp3'
-	      }
-	    ]
-	  },
-	  {
-	    name: 'Yellow Submarine',
-	    id: 2,
-	    imageUrl: 'http://fillmurray.com/300/300',
-	    songs: [
-	      {
-	        id: 4,
-	        name: 'London Calling',
-	        artists: [ 
-	          { name: 'Bill' } 
-	        ],
-	        genre: 'Punk',
-	        audioUrl: 'https://learndotresources.s3.amazonaws.com/workshop/5616dbe5a561920300b10cd7/Dexter_Britain_-_03_-_The_Stars_Are_Out_Interlude.mp3'
-	      }
-	    ]
-	  }
-	];
-
-
+import axios from "axios";
 	
 export default class Main extends React.Component  {
 	constructor(props) {
 		super(props);
 		this.state = {
-			albums: fakeAlbums
+			albums: []
 		};
+	}
+	componentWillMount(){
+		axios.get('/api/albums')
+		.then((response) => response.data)
+		.then((allAlbums)=> {
+			allAlbums = allAlbums.map((album)=> {
+				album.imageUrl = `/api/albums/${album.id}/image`;
+				return album;
+			})	
+			this.setState({albums: allAlbums})
+		})
+		.catch(e => console.log(e));
 	}
 	render(){
 		return(
@@ -77,4 +33,4 @@ export default class Main extends React.Component  {
 			</div>
 		)
 	}
-}
+}	
